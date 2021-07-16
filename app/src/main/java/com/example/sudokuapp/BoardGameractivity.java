42aa088470ae1635d6481f2a543f1e74731b9426
Talat Salmh <62280134+talat1976@users.noreply.github.com>
2021-07-16 15:32:02 +0300

    first commit

---
+++ app/src/main/java/com/example/sudokuapp/BoardGameractivity.java
@@ -0,0 +1,31 @@
+package com.example.sudokuapp;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class BoardGameractivity extends Activity {
+
+    private Button logout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bordgamer);
+       logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(BoardGameractivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+}
